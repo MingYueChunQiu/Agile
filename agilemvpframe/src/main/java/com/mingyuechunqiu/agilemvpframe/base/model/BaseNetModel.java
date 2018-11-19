abded5ebfb7d37e27e63200abc11b86778d83979
xiyujie <yujie.xi@ehailuo.com>
2018-11-19 11:21:06 +0800

    完善Model网络层功能

--- agilemvpframe/src/main/java/com/mingyuechunqiu/agilemvpframe/base/model/BaseNetModel.java
+++ agilemvpframe/src/main/java/com/mingyuechunqiu/agilemvpframe/base/model/BaseNetModel.java
@@ -1,6 +1,10 @@
 package com.mingyuechunqiu.agilemvpframe.base.model;
 
+import com.mingyuechunqiu.agilemvpframe.R;
 import com.mingyuechunqiu.agilemvpframe.base.framework.IBaseListener;
+import com.mingyuechunqiu.agilemvpframe.util.LogUtils;
+
+import retrofit2.Response;
 
 /**
  * <pre>
@@ -16,5 +20,22 @@ public abstract class BaseNetModel<I extends IBaseListener> extends BaseAbstract
 
     public BaseNetModel(I listener) {
         super(listener);
+    }
+
+    /**
+     * 检测Retrofit的网络响应是否为空
+     *
+     * @param response 网络响应
+     * @return 如果为空返回true，否则返回false
+     */
+    protected boolean checkResponseIsNull(Response response) {
+        if (response == null || response.body() == null) {
+            LogUtils.d(TAG_FAILURE, "服务器响应异常，请重试！");
+            if (mListener != null) {
+                mListener.onFailure(R.string.error_service_response);
+            }
+            return true;
+        }
+        return false;
     }
 }
