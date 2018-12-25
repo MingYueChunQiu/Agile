ef4daa0ca08e50ff3b5565d8556273d2fc5b4462
xiyujie <yujie.xi@ehailuo.com>
2018-12-25 16:47:12 +0800

    增加json与map之间转换

--- agilemvpframe/src/main/java/com/mingyuechunqiu/agilemvpframe/util/GsonUtils.java
+++ agilemvpframe/src/main/java/com/mingyuechunqiu/agilemvpframe/util/GsonUtils.java
@@ -1,10 +1,13 @@
 package com.mingyuechunqiu.agilemvpframe.util;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.text.TextUtils;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.mingyuechunqiu.agilemvpframe.io.BaseFile;
 
@@ -15,14 +18,17 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * <pre>
  *     author : xyj
  *     e-mail : yujie.xi@ehailuo.com
  *     time   : 2018/9/7
- *     desc   :
+ *     desc   : json转换工具类
  *     version: 1.0
  * </pre>
  */
@@ -71,6 +77,7 @@ public class GsonUtils extends BaseFile {
      * @param <T>      json对象泛型约束
      * @return 读取转换成功返回json对象，否则返回null
      */
+    @Nullable
     public static <T> T readJsonFromFile(String fileName, Class<T> c) {
         if (TextUtils.isEmpty(fileName)) {
             return null;
@@ -102,6 +109,7 @@ public class GsonUtils extends BaseFile {
      * @param <T>      json对象类型
      * @return 转换成功返回json对象，否则返回null
      */
+    @Nullable
     public static <T> List<T> readJsonArrayFromFile(String fileName, Class<T> c) {
         if (TextUtils.isEmpty(fileName)) {
             return null;
@@ -118,12 +126,7 @@ public class GsonUtils extends BaseFile {
             if (jsonArray == null) {
                 return null;
             }
-            List<T> list = new ArrayList<>();
-            Gson gson = new Gson();
-            for (JsonElement jsonElement : jsonArray) {
-                list.add(gson.fromJson(jsonElement, c));
-            }
-            return list;
+            return getListFromJson(jsonArray, c);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -132,6 +135,58 @@ public class GsonUtils extends BaseFile {
             closeStream(reader);
         }
         return null;
+    }
+
+    /**
+     * 将json字符串转换成list集合
+     *
+     * @param jsonArray json数组
+     * @param c         值的类型
+     * @param <T>       值的泛型类型
+     * @return 返回list集合
+     */
+    public static <T> List<T> getListFromJson(@NonNull JsonArray jsonArray, @NonNull Class<T> c) {
+        List<T> list = new ArrayList<>();
+        Gson gson = new Gson();
+        for (JsonElement jsonElement : jsonArray) {
+            list.add(gson.fromJson(jsonElement, c));
+        }
+        return list;
+    }
+
+    /**
+     * 将json字符串转换成map集合
+     *
+     * @param json json格式的字符串
+     * @param c    map值的类型
+     * @param <T>  值的泛型类型
+     * @return 返回map集合
+     */
+    @Nullable
+    public static <T> Map<String, Object> getMapFromJson(@NonNull String json, @NonNull Class<T> c) {
+        if (TextUtils.isEmpty(json)) {
+            return null;
+        }
+        JsonObject jsonObject = new JsonParser().parse(json).getAsJsonObject();
+        if (jsonObject == null) {
+            return null;
+        }
+        Gson gson = new Gson();
+        Map<String, Object> map = new HashMap<>();
+        Set<Map.Entry<String, JsonElement>> entries = jsonObject.entrySet();
+        for (Map.Entry<String, JsonElement> entry : entries) {
+            String key = entry.getKey();
+            JsonElement jsonElement = entry.getValue();
+            if (jsonElement instanceof JsonArray) {
+                List<T> list = getListFromJson(jsonElement.getAsJsonArray(), c);
+                if (list != null) {
+                    map.put(key, list);
+                }
+            } else if (jsonElement instanceof JsonObject) {
+                map.put(key, gson.fromJson(jsonElement, c));
+            }
+        }
+        return map;
     }
 
     /**
