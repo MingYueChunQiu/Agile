186a610f3384440e9b68c811058ec06b879748dc
xiyujie <yujie.xi@ehailuo.com>
2018-12-26 15:07:20 +0800

    优化软键盘与布局处理功能

--- agilemvpframe/src/main/java/com/mingyuechunqiu/agilemvpframe/framework/OnKeyBoardChangeListener.java
+++ agilemvpframe/src/main/java/com/mingyuechunqiu/agilemvpframe/framework/OnKeyBoardChangeListener.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.FrameLayout;
 
+import com.mingyuechunqiu.agilemvpframe.util.ScreenUtils;
+
 import java.lang.ref.WeakReference;
 
 /**
@@ -25,19 +27,27 @@ public class OnKeyBoardChangeListener {
     private View mChildOfContent;
     private int usableHeightPrevious;
     private FrameLayout.LayoutParams frameLayoutParams;
+    private boolean removeStatusHeight;//标记是否移除状态栏高度
 
     @NonNull
-    public static OnKeyBoardChangeListener getInstance(Activity activity) {
-        return new OnKeyBoardChangeListener(activity);
+    public static OnKeyBoardChangeListener getInstance(Activity activity, boolean removeStatusHeight) {
+        OnKeyBoardChangeListener listener = new OnKeyBoardChangeListener(activity);
+        listener.removeStatusHeight = removeStatusHeight;
+        return listener;
     }
 
     private OnKeyBoardChangeListener(Activity activity) {
         mActivityRef = new WeakReference<>(activity);
     }
 
-    /**
-     * 获取内容视图等初始化信息
-     */
+    public boolean isRemoveStatusHeight() {
+        return removeStatusHeight;
+    }
+
+    public void setRemoveStatusHeight(boolean removeStatusHeight) {
+        this.removeStatusHeight = removeStatusHeight;
+    }
+
     public void init() {
         if (mActivityRef.get() == null) {
             return;
@@ -58,6 +68,9 @@ public class OnKeyBoardChangeListener {
      * 重新布局内容视图
      */
     private void possiblyResizeChildOfContent() {
+        if (mActivityRef.get() == null) {
+            return;
+        }
         int usableHeightNow = computeUsableHeight();
         if (usableHeightNow != usableHeightPrevious) {
             int usableHeightSansKeyboard = mChildOfContent.getRootView()
@@ -67,6 +80,9 @@ public class OnKeyBoardChangeListener {
                 // keyboard probably just became visible
                 frameLayoutParams.height = usableHeightSansKeyboard
                         - heightDifference;
+                if (removeStatusHeight) {
+                    frameLayoutParams.height += ScreenUtils.getStatusBarHeight(mActivityRef.get());
+                }
             } else {
                 // keyboard probably just became hidden
                 frameLayoutParams.height = usableHeightSansKeyboard;
