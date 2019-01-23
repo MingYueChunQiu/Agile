3957aecfee537f0f4c7c6ff70e8e879d1b3d5583
xiyujie <yujie.xi@ehailuo.com>
2019-01-23 19:39:55 +0800

    完善LoadingFragment模块

--- agilemvpframe/src/main/java/com/mingyuechunqiu/agilemvpframe/base/presenter/BaseDialogPresenter.java
+++ agilemvpframe/src/main/java/com/mingyuechunqiu/agilemvpframe/base/presenter/BaseDialogPresenter.java
@@ -1,7 +1,12 @@
 package com.mingyuechunqiu.agilemvpframe.base.presenter;
 
+import android.support.annotation.IdRes;
+import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
+
 import com.mingyuechunqiu.agilemvpframe.base.model.BaseAbstractModel;
 import com.mingyuechunqiu.agilemvpframe.base.view.BaseDialogView;
+import com.mingyuechunqiu.agilemvpframe.feature.loadingFragment.LoadingFragmentOption;
 
 /**
  * <pre>
@@ -14,6 +19,28 @@ import com.mingyuechunqiu.agilemvpframe.base.view.BaseDialogView;
  * </pre>
  */
 public abstract class BaseDialogPresenter<V extends BaseDialogView, M extends BaseAbstractModel> extends BaseAbstractPresenter<V, M> {
+
+    /**
+     * 显示提示信息
+     *
+     * @param hint 提示文本
+     */
+    protected void showToast(String hint) {
+        if (mViewRef.get() != null) {
+            mViewRef.get().showToast(hint);
+        }
+    }
+
+    /**
+     * 显示提示信息
+     *
+     * @param stringResourceId 提示文本资源ID
+     */
+    protected void showToast(@StringRes int stringResourceId) {
+        if (mViewRef.get() != null) {
+            mViewRef.get().showToast(stringResourceId);
+        }
+    }
 
     /**
      * 显示加载对话框
@@ -34,6 +61,27 @@ public abstract class BaseDialogPresenter<V extends BaseDialogView, M extends Ba
     protected void disappearLoadingDialog() {
         if (mViewRef.get() != null) {
             mViewRef.get().disappearLoadingDialog();
+        }
+    }
+
+    /**
+     * 显示加载界面
+     *
+     * @param containerId 依附的父布局资源ID
+     * @param option      加载配置参数信息对象
+     */
+    protected void showLoadingFragment(@IdRes int containerId, @Nullable LoadingFragmentOption option) {
+        if (mViewRef.get() != null) {
+            mViewRef.get().showLoadingFragment(containerId, option);
+        }
+    }
+
+    /**
+     * 隐藏加载界面
+     */
+    protected void hideLoadingFragment() {
+        if (mViewRef.get() != null) {
+            mViewRef.get().hideLoadingFragment();
         }
     }
 
@@ -60,5 +108,4 @@ public abstract class BaseDialogPresenter<V extends BaseDialogView, M extends Ba
             mViewRef.get().disappearLoadingDialog();
         }
     }
-
 }
