50cc01e70a1c96b4edfafbbc2603b4bf426b2114
xiyujie <yujie.xi@ehailuo.com>
2019-06-27 14:34:30 +0800

    解决相关问题

---
+++ agile/src/main/java/com/mingyuechunqiu/agile/ui/widget/PageSlidingPanelLayout.java
@@ -0,0 +1,72 @@
+package com.mingyuechunqiu.agile.ui.widget;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.MotionEventCompat;
+import android.support.v4.widget.SlidingPaneLayout;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ViewConfiguration;
+
+/**
+ * <pre>
+ *     author : xyj
+ *     Github : https://github.com/MingYueChunQiu
+ *     e-mail : xiyujieit@163.com
+ *     time   : 2019/6/27
+ *     desc   : 自定义滑动面板（解决ViewPager和SlidingPaneLayout的滑动事件冲突）
+ *              继承自SlidingPaneLayout
+ *     version: 1.0
+ * </pre>
+ */
+public class PageSlidingPanelLayout extends SlidingPaneLayout {
+
+    private float mInitialMotionX;
+    private float mInitialMotionY;
+    private float mEdgeSlop;
+
+    public PageSlidingPanelLayout(@NonNull Context context) {
+        super(context);
+    }
+
+    public PageSlidingPanelLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PageSlidingPanelLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        ViewConfiguration config = ViewConfiguration.get(context);
+        mEdgeSlop = config.getScaledEdgeSlop();
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        switch (MotionEventCompat.getActionMasked(ev)) {
+            case MotionEvent.ACTION_DOWN: {
+                mInitialMotionX = ev.getX();
+                mInitialMotionY = ev.getY();
+                break;
+            }
+
+            case MotionEvent.ACTION_MOVE: {
+                final float x = ev.getX();
+                final float y = ev.getY();
+                // The user should always be able to "agile_close" the pane, so we only check
+                // for child scrollability if the pane is currently closed.
+                if (mInitialMotionX > mEdgeSlop && !isOpen() && canScroll(this, false,
+                        Math.round(x - mInitialMotionX), Math.round(x), Math.round(y))) {
+
+                    // How do we set super.mIsUnableToDrag = true?
+
+                    // send the parent a cancel event
+                    MotionEvent cancelEvent = MotionEvent.obtain(ev);
+                    cancelEvent.setAction(MotionEvent.ACTION_CANCEL);
+                    return super.onInterceptTouchEvent(cancelEvent);
+                }
+            }
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+}
+
