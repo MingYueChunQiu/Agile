24a48a426e288c559da1819c593df2cd22ee5069
xiyujie <xiyujieit@163.com>
2020-01-20 17:33:00 +0800

    增加功能

---
+++ agile/src/main/java/com/mingyuechunqiu/agile/base/presenter/engine/countdown/ITimingPresenterEngine.kt
@@ -0,0 +1,53 @@
+package com.example.demo.countdown
+
+import com.mingyuechunqiu.agile.base.presenter.engine.IBasePresenterEngine
+
+/**
+ * <pre>
+ *      Project:    Demo
+ *      Github :    https://github.com/MingYueChunQiu
+ *      Author:     XiYuJie
+ *      Email:      xiyujie@jinying.com
+ *      Time:       2020/1/20 16:57
+ *      Desc:       Present层计时引擎接口
+ *                  继承自IBasePresenterEngine
+ *      Version:    1.0
+ * </pre>
+ */
+interface ITimingPresenterEngine : IBasePresenterEngine {
+
+    /**
+     * 开启一个延迟任务
+     *
+     * @param delay 延迟时长（毫秒数）
+     * @param task  任务
+     */
+    fun startDelayTask(delay: Long, task: Runnable)
+
+    /**
+     * 开启一个延迟计时任务
+     *
+     * @param delay     延迟时长（毫秒数）
+     * @param period    间隔时长（毫秒数）
+     * @param task      任务
+     */
+    fun startDelayPeriodTask(delay: Long, period: Long, task: Runnable)
+
+    fun startCountDownTask(count: Int, callback: CountDownCallback)
+
+    companion object {
+
+        interface CountDownCallback {
+
+            /**
+             * 每次倒计时刷新时回调
+             */
+            fun onEachCountDown(remainder: Int)
+
+            /**
+             * 当完后倒计时时回调
+             */
+            fun onCompleteCountDown()
+        }
+    }
+}
