d333bbd1ae31766ead8fe5104a8c1570da9757db
xiyujie <xiyujieit@163.com>
2020-10-17 10:23:34 +0800

    optimize BitmapUtils and JsonManager

--- agile/src/main/java/com/mingyuechunqiu/agile/feature/json/IJsonManager.java
+++ agile/src/main/java/com/mingyuechunqiu/agile/feature/json/IJsonManager.java
@@ -21,9 +21,23 @@ public interface IJsonManager extends IJsonHelper {
 
     void setJsonHelper(@NonNull IJsonHelper helper);
 
+    /**
+     * 从原始资源文件获取Json
+     *
+     * @param context 上下文
+     * @param id      资源ID
+     * @return 如果获取成功，返回json字符串，否则返回null
+     */
     @Nullable
     String getJsonFromRawFile(@NonNull Context context, @RawRes int id);
 
+    /**
+     * 从资产文件获取Json
+     *
+     * @param context  上下文
+     * @param fileName 文件名称
+     * @return 如果获取成功，返回json字符串，否则返回null
+     */
     @Nullable
     String getJsonFromAssetFile(@NonNull Context context, @NonNull String fileName);
 }
