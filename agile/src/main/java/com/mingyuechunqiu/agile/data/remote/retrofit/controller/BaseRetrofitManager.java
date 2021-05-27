23cbac3171b522d94446a10f02d364723402356d
xiyujie <xiyujieit@163.com>
2021-05-27 21:51:37 +0800

    optimize code

--- agile/src/main/java/com/mingyuechunqiu/agile/data/remote/retrofit/controller/BaseRetrofitManager.java
+++ agile/src/main/java/com/mingyuechunqiu/agile/data/remote/retrofit/controller/BaseRetrofitManager.java
@@ -10,7 +10,6 @@ import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
@@ -37,7 +36,6 @@ public abstract class BaseRetrofitManager {
                 .baseUrl(getBaseUrl())
                 .client(getDefaultOkHttpClient())
                 .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .build();
     }
 
